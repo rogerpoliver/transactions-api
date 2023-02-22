3cefba2999854acb4b40e1e62d272d5a535f7b60
Roger Oliveira <rogerpolvr@gmail.com>
2023-02-22 12:27:09 -0300

    feat: add fastify api

---
+++ src/server.ts
@@ -0,0 +1,26 @@
+import fastify from 'fastify'
+import crypto from 'node:crypto'
+import { knex } from './database'
+import { env } from './env'
+
+const app = fastify()
+
+app.get('/hello', async () => {
+  const transaction = await knex('transactions')
+    .insert({
+      id: crypto.randomUUID(),
+      title: 'Transação de teste',
+      amount: 1000,
+    })
+    .returning('*')
+
+  return transaction
+})
+
+app
+  .listen({
+    port: Number(env.PORT),
+  })
+  .then(() => {
+    console.log('Server running at 3333')
+  })
